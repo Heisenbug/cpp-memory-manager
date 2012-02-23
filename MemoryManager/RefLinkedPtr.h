282a6ae79de4259281c0b98a8b67802210ac28ba
devis.rossini@gmail.com <devis.rossini@gmail.com@933dec87-be11-808a-fee0-ee9c17b7d01d>
2012-02-23 23:46:50 +0000

    Implemented RefLinkedPtr.

---
+++ MemoryManager/RefLinkedPtr.h
@@ -0,0 +1,125 @@
+#ifndef REFLINKEDPTR_H_INCLUDE_GUARD
+#define REFLINKEDPTR_H_INCLUDE_GUARD
+
+namespace MM
+{
+	template<typename T>
+	class RefLinkedPtr
+	{
+	public:
+
+		typedef T	value_type;
+		typedef T*	pointee_type;
+		typedef T*	pointer;
+		typedef T&	reference;
+
+		explicit RefLinkedPtr(pointer p = 0)
+			: mPointee(p)
+		{
+			mPrev = mNext = this;
+		}
+
+		RefLinkedPtr(const RefLinkedPtr& ptr)
+		{
+			Acquire(ptr);
+		}
+
+		RefLinkedPtr& operator=(const RefLinkedPtr& ptr)
+		{
+			if (this != &ptr)
+			{
+				Release();
+				Acquire(ptr);
+			}
+
+			return *this;
+		}
+
+		~RefLinkedPtr()
+		{
+			Release();
+		}
+
+		template<typename U> friend class RefLinkedPtr<U>;
+
+		template<typename U> 
+		RefLinkedPtr(const RefLinkedPtr<U>& ptr)
+		{
+			Acquire(ptr);
+		}
+
+		template<typename U> 
+		RefLinkedPtr& operator=(const RefLinkedPtr<U>& ptr)
+		{
+			if (this != &ptr) {
+				Release();
+				Acquire(ptr);
+			}
+
+			return *this;
+		}
+
+		reference operator*() const 
+		{
+			return *mPointee;
+		}
+
+		pointer operator->() const 
+		{
+			return mPointee;
+		}
+
+		pointer Get() const 
+		{
+			return mPointee;
+		}
+
+		bool IsUnique() const
+		{
+			return (mPointee ? mPrev == this : true);
+		}
+
+	private:
+
+		void Acquire(const RefLinkedPtr& ptr)
+		{ 
+			mPointee		= ptr.mPointee;
+			mNext			= ptr.mNext;
+			mNext->mPrev	= this;
+			mPrev			= &ptr;
+			ptr.mNext		= this;
+		}
+
+		template<typename U> 
+		void Acquire(const RefLinkedPtr<U>& ptr)
+		{ 
+			mPointee		= ptr.mPointee;
+			mNext			= ptr.mNext;
+			mNext->mPrev	= this;
+			mPrev			= &ptr;
+			ptr.mNext		= this;
+		}
+
+		void Release()
+		{ 
+			if (IsUnique()) 
+			{ 
+				delete mPointee;
+			}
+			else 
+			{
+				mPrev->mNext = mNext
+					mNext->mPrev = mPrev;
+				mPrev = mNext = 0;
+			}
+
+			mPointee = 0;
+		}
+
+		pointer						mPointee;
+		mutable const RefLinkedPtr* mPrev;
+		mutable const RefLinkedPtr* mNext;
+	};
+}
+
+#endif // REFLINKEDPTR_H_INCLUDE_GUARD
---
+++ MemoryManager/SmartPointers.h
@@ -0,0 +1,8 @@
+#ifndef SMARTPOINTERS_H_INCLUDE_GUARD
+#define SMARTPOINTERS_H_INCLUDE_GUARD
+
+#include "OwnedPtr.h"
+#include "RefCountedPtr.h"
+#include "RefLinkedPtr.h"
+
+#endif // SMARTPOINTERS_H_INCLUDE_GUARD
