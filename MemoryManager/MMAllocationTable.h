876fb2f9871c77cc00f666811158b20650804a72
devis.rossini@gmail.com <devis.rossini@gmail.com@933dec87-be11-808a-fee0-ee9c17b7d01d>
2012-02-25 10:07:36 +0000

    First version of the AllocationTable.

--- MemoryManager/MMAllocationTable.h
+++ MemoryManager/MMAllocationTable.h
@@ -8,9 +8,13 @@
 
 #include <utility>
 #include <map>
+#include <cassert>
 
 namespace MM
 {
+	// Chunk index is the key type used for the AllocationTable
+	#pragma region ChunkIndex
+
 	struct ChunkIndex 
 	{
 		ChunkIndex(ChunkInterface* p, size_t size = 0)
@@ -19,15 +23,63 @@ namespace MM
 		std::pair<ChunkInterface*, size_t> mIndex;
 	};
 
+	#pragma region Comparsion operators
+
 	bool operator==(const ChunkIndex& i1, const ChunkIndex& i2)
 	{
+		// Suppose i1 = <p, s> and i2 = <q, t>
+
+		// If s == 0 || t == 0
+		if (i1.mIndex.second == 0 || i2.mIndex.second == 0)
+		{
+			// Case s == 0
+			if (i1.mIndex.first == 0)
+			{
+				// Check if (p >= q && p < q + t)
+				return (i1.mIndex.first >= i2.mIndex.first &&
+							i1.mIndex.first < i2.mIndex.first + i2.mIndex.second);
+			}
+			else
+			{
+				// Case t == 0
+
+				// Check if (q >= p && q < p + s)
+				return (i2.mIndex.first >= i1.mIndex.first &&
+							i2.mIndex.first < i1.mIndex.first + i1.mIndex.second);
+			}
+		}
+		
 		return (i1 == i2);
 	}
 
 	bool operator<(const ChunkIndex& i1, const ChunkIndex& i2)
 	{
-
+		return i1.mIndex.first < i2.mIndex.first;
 	}
+
+	bool operator>(const ChunkIndex& i1, const ChunkIndex& i2)
+	{
+		return i1.mIndex.first > i2.mIndex.first;
+	}
+
+	bool operator<=(const ChunkIndex& i1, const ChunkIndex& i2)
+	{
+		return (i1 < i2 || i1 == i2);
+	}
+
+	bool operator>=(const ChunkIndex& i1, const ChunkIndex& i2)
+	{
+		return (i1 > i2 || i1 == i2);
+	}
+
+	bool operator!=(const ChunkIndex& i1, const ChunkIndex& i2)
+	{
+		return !(i1 == i2);
+	}
+
+	#pragma endregion
+
+	#pragma endregion 
 
 	typedef std::map<ChunkIndex, AllocatorInterface*> AllocTable; 
 
@@ -40,7 +92,21 @@ namespace MM
 			SingletonHolder<AllocTable>::Instance().insert( ChunkIndex(c, size) );
 		}
 
-		// Implement a search function using std::map::find() method
+		static AllocatorInterface* FindAllocatorFor(void* p)
+		{
+			// TODO: This segment is NOT thread-safe! (A multiple acces can invalidate the calculus of end())
+			AllocTable::iterator res = 
+				SingletonHolder<AllocTable>::Instance().find( ChunkIndex(reinterpret_cast<ChunkInterface*>(p)) );
+
+			if (res != SingletonHolder<AllocTable>::Instance().end())
+			{
+				return res->second;
+			}
+
+			// "You shall not pass!" (Gandalf, LOTR) XD
+			assert(false);
+			return 0;
+		}
 	};
 }
 
